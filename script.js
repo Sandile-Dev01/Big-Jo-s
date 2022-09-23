9334ba821439942539d7716ef577816565bd5e95
Sandile Ndimande <sandilegofx@gmail.com>
2022-09-23 12:47:47 +0200

    added the ads carousel

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+
+//Animated ads carousel
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("animatedAds");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
 //Recommendations
 
 let shop = document.getElementById("recommended");
@@ -61,6 +91,8 @@ let calculation = () => {
 };
 
 calculation();
+
+
 
 
 //Burgers
