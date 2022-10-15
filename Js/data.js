ce31a00a35d358cdc8f8e21f72cc1ab7d234ab5c
Sandile Ndimande <sandilegofx@gmail.com>
2022-10-15 23:30:11 +0200

    added some data

--- Js/data.js
+++ Js/data.js
@@ -8,18 +8,18 @@ let shopItemsData = [{
     id: "hakeFam",
     name: "Hake Family Feast",
     price: 250,
-    img: "../Hake/HakeFeast.png"
+    img: "./Hake/HakeFeast.png"
 },
 {
     id: "BurgerCombo",
     name: "3 Way Burger Combo and Chips",
     price: 200,
-    img: "../Burgers/ThreeWay.png"
+    img: "/Burgers/ThreeWay.png"
 },
 {
     id: "hotWingsFeast",
     name: "Hot Wings Feast and Chips",
     price: 190,
-    img: "../Chicken/hotWingsFeast.png"
+    img: "Chicken/hotWingsFeast.png"
 }
 ];
