defbf35c74b77b5f0c7ef1dac8c2ed4358768a6d
Sandile Ndimande <sandilegofx@gmail.com>
2022-10-17 14:49:56 +0200

    added some files

--- Js/script.js
+++ Js/script.js
@@ -32,6 +32,32 @@ function showSlides(n) {
 
 let shop = document.getElementById("recommended");
 
+let shopItemsData = [{
+  id: "tummyPleaser",
+  name: "Tummy Pleaser and Chips",
+  price: 120,
+  img: "../Burgers/TummyPleaser.png"
+},
+{
+  id: "hakeFam",
+  name: "Hake Family Feast",
+  price: 250,
+  img: "../Hake/HakeFeast.png"
+},
+{
+  id: "BurgerCombo",
+  name: "3 Way Burger Combo and Chips",
+  price: 200,
+  img: "../Burgers/ThreeWay.png"
+},
+{
+  id: "hotWingsFeast",
+  name: "Hot Wings Feast and Chips",
+  price: 190,
+  img: "../Chicken/hotWingsFeast.png"
+}
+];
+
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 let generateShop = () => {
