c1e72c737030e28a625e1b6ca6414ccf6f1d3987
Sandile Ndimande <sandilegofx@gmail.com>
2022-10-17 15:02:16 +0200

    added a file

--- Js/script.js
+++ Js/script.js
@@ -36,25 +36,25 @@ let shopItemsData = [{
   id: "tummyPleaser",
   name: "Tummy Pleaser and Chips",
   price: 120,
-  img: "../Burgers/TummyPleaser.png"
+  img: "/Burgers/TummyPleaser.png"
 },
 {
   id: "hakeFam",
   name: "Hake Family Feast",
   price: 250,
-  img: "../Hake/HakeFeast.png"
+  img: "/Hake/HakeFeast.png"
 },
 {
   id: "BurgerCombo",
   name: "3 Way Burger Combo and Chips",
   price: 200,
-  img: "../Burgers/ThreeWay.png"
+  img: "/Burgers/ThreeWay.png"
 },
 {
   id: "hotWingsFeast",
   name: "Hot Wings Feast and Chips",
   price: 190,
-  img: "../Chicken/hotWingsFeast.png"
+  img: "/Chicken/hotWingsFeast.png"
 }
 ];
 
